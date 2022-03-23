c187171047627d8676f247a58bbdbaa4a5663a31
Gopikrishna Rathinavel <rgopikrishna@vt.edu>
2022-03-23 17:55:43 -0400

    someroutes

--- application.py
+++ application.py
@@ -5,3 +5,13 @@ application = Flask(__name__)
 @application.route('/')
 def hello_world():
     return 'Sup.'
+
+
+@app.route("/hi/")
+def who():
+    return "Who are you?"
+
+
+@app.route("/hi/<username>")
+def greet(username):
+    return f"Hi there, {username}!"
